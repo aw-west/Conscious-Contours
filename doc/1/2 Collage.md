bf00d15f0fa4c090c996cca3ada089b957cbc5f1
aw-west <>
2020-05-31 11:33:58 +0100

    concious contours

--- doc/1/2 Collage.md
+++ doc/1/2 Collage.md
@@ -1,4 +1,4 @@
-# <!-- This is here so contents links don't lose order -->
+#
 
 ![Untitled One](img/Untitled-One.jpg "Untitled One")
 ### Mixed Media
--- doc/md2html.py
+++ doc/md2html.py
@@ -11,6 +11,7 @@ line = lambda z: re.sub('\n|\r|\t', '', z)
 # Articles
 articles = []
 for i, f in enumerate(os.listdir(volume)):
+	if i==0:  i-=1
 	article = pypandoc.convert_file(volume+'/'+f, to='html5', format='markdown_strict+hard_line_breaks')
 	article = re.sub(' id="([a-z]|-)*"', '', article)
 	article = tag('article', f" id='#{volume}-{i+2}'", article)
@@ -21,7 +22,7 @@ for i, f in enumerate(os.listdir(volume)):
 contents = re.findall('<h1>(.*?)</h1>', ''.join(articles))
 contents = [f'''<h4><a href="javascript:display('\u0023{volume}-{i+2}')">''' + content + '</a></h4>' for i, content in enumerate(contents)]
 contents = '<h1>Contents</h1>' + ''.join(contents)
-contents = tag('article', f" id='#{volume}-1'", contents)
+contents = tag('article', f" id='#{volume}-2'", contents)
 contents = line(contents)
 
 # Front Cover
@@ -41,6 +42,6 @@ for i in range(2+len(articles)):
 	buttons.append(button)
 buttons = tag('div', '', line(''.join(buttons)))
 buttons = line(buttons)
+html = '\n'.join([front, articles[0], contents, *articles[1:], buttons])
 
-html = '\n'.join([front, contents, *articles, buttons])
 print(html, file=open(f'{volume}.html',mode='w',encoding='utf-8'))
