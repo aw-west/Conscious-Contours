510d53b53edfb95d647b98e15138a252d221376f
aw-west <andy_west001@hotmail.co.uk>
2023-05-10 19:12:20 +0100

    improvements to webp

--- src/cwebp.sh
+++ src/cwebp.sh
@@ -1,3 +1,3 @@
 # Does not make directories
-mkdir ./docs/www/1 ./docs/www/2
-find ./src/img | egrep '.jpeg|.jpg|.tiff|.tif|.png' | cut -d/ -f4- | parallel --progress 'cwebp -quiet -af -o ./docs/www/{.}.webp -- ./src/www/{}'
+find ./src/img/ -type d | sed 's/src\/img/docs\/www/g' | xargs mkdir -p
+find src/img | egrep '.jpeg|.jpg|.tiff|.tif|.png' | cut -d/ -f3- | parallel --progress 'cwebp -quiet -af -resize 1200 0 ./src/img/{} -o ./docs/www/{.}.webp'
