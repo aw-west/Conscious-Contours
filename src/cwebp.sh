4f4659cdec58b3db458ff0bd04f5cf41e9ecbea1
aw-west <andy_west001@hotmail.co.uk>
2023-05-10 19:12:45 +0100

    does mkdirs now

--- src/cwebp.sh
+++ src/cwebp.sh
@@ -1,3 +1,2 @@
-# Does not make directories
 find ./src/img/ -type d | sed 's/src\/img/docs\/www/g' | xargs mkdir -p
 find src/img | egrep '.jpeg|.jpg|.tiff|.tif|.png' | cut -d/ -f3- | parallel --progress 'cwebp -quiet -af -resize 1200 0 ./src/img/{} -o ./docs/www/{.}.webp'
